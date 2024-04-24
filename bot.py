cdb533cf64b608d442cdb5a3153ebfbc0e01e6bd
Rahul0272 <105348416+Rahul0272@users.noreply.github.com>
2024-04-24 10:49:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -354,8 +354,7 @@ async def button(bot: Client, cmd: CallbackQuery):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("Updates Channel", url="
-https://t.me/+ERAfSWOWKk1mNjE1")
+                        InlineKeyboardButton("Updates Channel", url="https://t.me/+ERAfSWOWKk1mNjE1")
                     ],
                     [
                         InlineKeyboardButton("About Bot", callback_data="aboutbot"),
